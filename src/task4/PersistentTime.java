9b42d58481de1fd3bfa6a104506494d63aff2b6c
Paul Miller <paul_john.miller0008@stud.hkr.se>
2023-05-28 19:11:06 +0200

    task4 initial

---
+++ src/task4/PersistentTime.java
@@ -0,0 +1,15 @@
+package task4;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class PersistentTime implements Serializable {
+    private Date time;
+    public PersistentTime() {
+        time = Calendar.getInstance().getTime();
+    }
+    public Date getTime() {
+        return time;
+    }
+}
